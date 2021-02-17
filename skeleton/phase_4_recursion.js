cc2b7ed6092f242f0609d16a5b3d391d32d36cbb
Ray <raysidarta@gmail.com>
2021-02-17 17:17:22 -0500

    Work on phase 4 fibonacci

--- skeleton/phase_4_recursion.js
+++ skeleton/phase_4_recursion.js
@@ -19,3 +19,32 @@ function exponent(base, exp) {
 }
 
 console.log(exponent(2, 5));
+
+
+
+function exponent_2(base, exp){
+    if (exp === 0) { return 1; }
+    if (exp===1){return base;}
+    if (exp%2 === 0){return exponent_2(base,exp/2)**2;}
+    else{return base*exponent_2(base, (exp-1)/2)**2;}
+}
+console.log(exponent_2(2, 6));
+console.log(exponent_2(2, 7));
+
+
+
+
+function fibonacci(n){
+    if (n ===1){return [1]; }
+    if (n===2){return [1,1];}
+    if (n <1){return [];}
+
+    return fibonacci(n-1).push(2);  //.push(  fibonacci(n-1)[n-2] +fibonacci(n-1)[n-1] );
+}
+console.log(fibonacci(5));
+
+
+
+
+
+
