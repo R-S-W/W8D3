d943fc23730f6ad98d0005ba843ef6e2fddfe80d
John Cigale <jcigale98@gmail.com>
2021-02-17 14:45:19 -0500

    transpose and twoSum

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,8 +1,8 @@
 
 
-function uniq(array){
+Array.prototype.uniq = function() {
     let new_array = [];
-    array.forEach(  function(ele) {
+    this.forEach(  function(ele) {
         if (!new_array.includes(ele)){
             new_array.push(ele);
         }
@@ -11,4 +11,36 @@ function uniq(array){
     return new_array;
 }
 
-console.log(uniq([1,1,4,4,4,3,2,2,2]));
+console.log([1,1,4,4,4,3,2,2,2].uniq());
+
+Array.prototype.twoSum = function() {
+    let pairArray = [];
+    for(let i = 1; i < this.length; i++) {
+        for(let j = 0; j < i; j++) {
+            if(this[i] + this[j] === 0) {
+                pairArray.push([j, i]);
+            }
+        }
+    }
+    return pairArray;
+}
+
+console.log([1, -1, 2, -2, 3, 4, -3, 0, 0].twoSum());
+
+
+Array.prototype.transpose = function() {
+    let newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        newArray.push([])
+        for (let j = 0; j < this.length; j++) {
+            newArray[newArray.length - 1].push(this[j][i]);
+        }
+    }
+    return newArray;
+}
+
+console.log(
+   [[1,2,3],
+    [4,5,6],
+    [7,8,9]].transpose()
+);
